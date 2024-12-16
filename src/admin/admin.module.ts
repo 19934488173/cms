85949940a6c62324582a9eac21fc1bf2232c1a55
amberHe <1542588809@qq.com>
2024-12-16 15:26:06 +0800

    资源

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { DashboardController } from './controllers/dashboard.controller';
 import { UserController } from './controllers/user.controller';
 import { AdminExceptionFilter } from './filters/admin-exception.filter';
-
+import { RoleController } from './controllers/role.controller';
+import { AccessController } from './controllers/access.controller';
 @Module({
-  controllers: [DashboardController, UserController],
+  controllers: [
+    DashboardController,
+    UserController,
+    RoleController,
+    AccessController,
+  ],
   providers: [
     {
       provide: 'APP_FILTER',
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -6,6 +6,10 @@ import { User } from './entities/user.entity';
 import { UserService } from './services/user.service';
 import { UtilityService } from './services/utility.service';
 import { IsUsernameUniqueConstraint } from './validators/user-validators';
+import { Role } from './entities/role.entity';
+import { RoleService } from './services/role.service';
+import { Access } from './entities/access.entity';
+import { AccessService } from './services/access.service';
 @Global()
 @Module({
   imports: [
@@ -21,19 +25,23 @@ import { IsUsernameUniqueConstraint } from './validators/user-validators';
       }),
     }),
     //注册特定实体（如 User）的 Repository，用于操作对应的数据库表
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Role, Access]),
   ],
   providers: [
     ConfigurationService,
     UserService,
     UtilityService,
     IsUsernameUniqueConstraint,
+    RoleService,
+    AccessService,
   ],
   exports: [
     ConfigurationService,
     UserService,
     UtilityService,
     IsUsernameUniqueConstraint,
+    RoleService,
+    AccessService,
   ],
 })
 export class SharedModule {}
